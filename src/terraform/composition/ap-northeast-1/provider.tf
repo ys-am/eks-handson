ae0c64dc1cf518b6c801d74108fbcfd7ab2d399b
qlow610 <schwarzleiche@gmail.com>
2023-01-15 15:39:50 +0900

    k8s api version up

--- src/terraform/composition/ap-northeast-1/provider.tf
+++ src/terraform/composition/ap-northeast-1/provider.tf
@@ -18,7 +18,7 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(module.guardian.cluster_certificate_authority_data)
 
   exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
+    api_version = "client.authentication.k8s.io/v1"
     command     = "aws"
     #This requires the awscli to be installed locally where Terraform is executed
     args = ["eks", "get-token", "--cluster-name", module.guardian.cluster_name]
