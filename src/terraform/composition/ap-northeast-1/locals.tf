71da5c4519f5e084f9861c3ef15e1a4a24dc854a
qlow610 <schwarzleiche@gmail.com>
2023-01-15 12:06:03 +0900

    fix network subnet

--- src/terraform/composition/ap-northeast-1/locals.tf
+++ src/terraform/composition/ap-northeast-1/locals.tf
@@ -1,3 +1,3 @@
 locals {
-  cidr_blocks = [for cidr_block in cidrsubnets("${var.cidr}", 8, 8, 8) : cidrsubnets(cidr_block, 4, 4, 4)]
+  cidr_blocks = [for cidr_block in cidrsubnets("${var.cidr}", 4, 4, 4) : cidrsubnets(cidr_block, 4, 4, 4)]
 }
--- src/terraform/composition/ap-northeast-1/variable.tf
+++ src/terraform/composition/ap-northeast-1/variable.tf
@@ -1,6 +1,6 @@
 variable "cidr" {
   type    = string
-  default = "10.0.0.0/16"
+  default = "10.0.0.0/8"
 }
 
 variable "AWS_ACCESS_KEY_ID" {
