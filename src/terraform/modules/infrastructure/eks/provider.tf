345a179f0b0d7f0dafacc26ff92a78bcad6e9b6a
qlow610 <schwarzleiche@gmail.com>
2023-01-15 16:36:07 +0900

    Revert "test k8s config"
    This reverts commit fa4b8d16d2c39761d73ffcfa17d2c40f6cdd0bbc.

--- src/terraform/modules/infrastructure/eks/provider.tf
+++ src/terraform/modules/infrastructure/eks/provider.tf
@@ -8,17 +8,21 @@ terraform {
       source  = "hashicorp/kubernetes"
       version = "2.16.1"
     }
+    local = {
+      source  = "hashicorp/local"
+      version = "2.3.0"
+    }
   }
 }
 
-# provider "kubernetes" {
-#   host                   = module.eks.cluster_endpoint
-#   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
 
-#   exec {
-#     api_version = "client.authentication.k8s.io/v1beta1"
-#     command     = "aws"
-#     # This requires the awscli to be installed locally where Terraform is executed
-#     args = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
-#   }
-# }
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    # This requires the awscli to be installed locally where Terraform is executed
+    args = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+  }
+}
